9086c0f43b166cd517ced79a1aa42fb7e9e95e3e
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2024-11-19 13:16:40 -0500

    Book list screen - Search bar component
    - Create BookListScreen to manage the components that are handled by the main book list screen
    - Create the BookSearchBar component which is rendered in the BookListScreen.
    - Create the BookListAction to manage the actions related to the BookListScreen
    - Creare the BookListState to be able to manage the states that are linked to the Book list screen
    - Create the BookListViewModel, which is the bridge between the view and the data layer. Here the actions, the states are filled to be used in the BookListScree
    - Add custom strings to the androidMain
    - Refactor the App.kt to see how is rendered the UI in the book list screen using the main activity (Android)

---
+++ composeApp/src/commonMain/kotlin/com/sandobookpedia/book/presentation/book_list/BookListState.kt
@@ -0,0 +1,13 @@
+package com.sandobookpedia.book.presentation.book_list
+
+import com.sandobookpedia.book.domain.Book
+import com.sandobookpedia.core.presentation.UiText
+
+data class BookListState(
+    val searchQuery: String = "Kotlin",
+    val searchResults: List<Book> = emptyList(),
+    val favoriteBooks: List<Book> = emptyList(),
+    val isLoading: Boolean = true,
+    val selectedTabIndex: Int = 0,
+    val errorMessage: UiText? = null
+)
